728a4b18edad38500cb8bdbd4c4fa7f9956fb347
Ebriopes <antonio.vargasrosales@gmail.com>
2021-12-12 03:35:19 -0600

    feat: add user info to the checkout page

--- src/Views/Checkout.js
+++ src/Views/Checkout.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import Subtotal from '../components/Subtotal';
-import './Checkout.css';
-import CheckoutProduct from '../components/CheckoutProduct';
-import { useStateValue } from '../contexts/StateProvidder';
+import React from 'react'
+import Subtotal from '../components/Subtotal'
+import './Checkout.css'
+import CheckoutProduct from '../components/CheckoutProduct'
+import { useStateValue } from '../contexts/StateProvidder'
 
 const Checkout = () => {
-	const [ { basket } ] = useStateValue();
+  const [{ user, basket }] = useStateValue()
 
-	return (
-		<div className="checkout">
-			<div className="checkout_left">
-				<div>
-					<h2 className="checkout_title"> Your basket shop</h2>
-					<br />
-					{basket.map( ( item ) => (
-						<CheckoutProduct
-							key={Math.random( item.id )}
-							id={item.id}
-							image={item.image}
-							price={item.price}
-							rating={item.rating}
-							title={item.title}
-						/> ) )}
-				</div>
-			</div>
-			<div className="checkour_right">
-				<Subtotal />
-			</div>
-		</div>
-	);
-};
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <div>
+          <h3>Hello {user?.email}</h3>
+          <h2 className="checkout_title"> Your basket shop</h2>
+          <br />
+          {basket.map(item => (
+            <CheckoutProduct
+              key={Math.random(item.id)}
+              id={item.id}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              title={item.title}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="checkour_right">
+        <Subtotal />
+      </div>
+    </div>
+  )
+}
 
-export default Checkout;
+export default Checkout
